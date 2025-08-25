b30d5c888981d871e3f0937bc94e6cbef44e0ed2
Prabin Rag TK <prabinrag2340@gmail.com>
2025-08-26 02:25:14 +0400

    Printing different data types

---
+++ printing.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>First PHP</title>
+</head>
+<body>
+<?php
+$a='abc';
+$b='a';
+$c=1;
+$d=1.1;
+$e=true;
+echo "Printing different data types <br>";
+echo gettype($a) . "<br>";
+echo gettype($b) . "<br>";
+echo gettype($c) . "<br>";
+echo gettype($d) . "<br>";
+echo gettype($e) . "<br>";
+echo "this is both valiable and string $a";
+?>
+</body>
+</html>
